1cf0e04ed46f4e1820f83081d4fe3f1de694e0b4
Mark Horrocks <mr.horrocks@gmail.com>
2024-06-13 05:56:08 +0100

    dynamic title added

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
-    <title>TITLE HERE</title>
+    <title><?php echo esc_html(get_bloginfo('name')); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
